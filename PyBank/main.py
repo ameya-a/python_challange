fe0e66471abca00580955a19e84170aa4f31dc07
ameya-a <152837210+ameya-a@users.noreply.github.com>
2024-01-08 23:59:25 -0500

    Module 3

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,66 +2,68 @@ import os
 import csv
 
 
+#joining path
 csvpath = os.path.join('Resources1','budget_data.csv')
-
-months = []
-PL = []
-averageChange = []
-#greatest_decrease = ["", 9999999]
-#greatest_increase = ["", 0]
-
 text_path = "output.txt"
 
+# open and read csv
 with open(csvpath) as csvfile:
-
-    # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-    # Read the header row first
-    csv_header = next(csvreader)
-
-
-    #The total number of months included in the dataset
-    total_months = len(months)
-       
-  
-    #The net total amount of "Profit/Losses" over the entire period
-    for row in csvreader:
-        PL.append(int(row[1]))
-        months.append(row[0])
-        
-#The changes in "Profit/Losses" over the entire period, and then the average of those changes
-    for x in range(1, len(PL)):
-        averageChange.append((int(PL[x]) - int(PL[x-1])))
-        
-    #Average = group/count
-    averageChange = sum(averageChange)/ len(averageChange)
-       
-
-#The greatest increase in profits (date and amount) over the entire period
-    greatestIncrease = max(averageChange)
+    csv_header = next(csvfile)
     
+    
+    PL = []
+    months = []
 
-#The greatest decrease in profits (date and amount) over the entire period
-    greatestDecrease = min(averageChange)
+    #skip header
+    for rows in csvreader:
+        PL.append(int(rows[1]))
+        months.append(rows[0])
+
+    # The total number of months included in the dataset
+    total_months = len(months)
+    
+    # revenue change
+    pl_change = []
 
 
+    for x in range(1, len(PL)):
+        pl_change.append((int(PL[x]) - int(PL[x-1])))
+    
+    # The changes in "Profit/Losses" over the entire period, and then the average of those changes
+    total_average = sum(pl_change) / len(pl_change)
+    
+  
 
-    print("Financial Analysis\n")
+    # The greatest increase in profits (date and amount) over the entire period
+    greatest_increase = max(pl_change)
+    # The greatest decrease in profits (date and amount) over the entire period
+    greatest_decrease = min(pl_change)
+
+
+    # print the Results
+    print("Financial Analysis")
+
     print("----------------------------")
-    print("Total Months: " + str(total_months))
+
+    print("total months: " + str(total_months))
+
     print("Total: " + "$" + str(sum(PL)))
-    print("Average Change: " + "%" + str(averageChange))
-    print("Greatest Increase in Profits: " + str(months[averageChange.index(max(averageChange))+1]) + " " + "$" + str(greatest_increase))
-#print("Greatest Decrease In Profits: " + str(months[averageChange.index(min(averageChange))+1]))+ " " + "$" + str(greatestDecrease))
+
+    print("Average change: " + "$" + str(total_average))
+
+    print("Greatest Increase in Profits: " + str(months[pl_change.index(max(pl_change))+1]) + " (" + "$" + str(greatest_increase) + ")" )
+
+    print("Greatest Decrease in Profits: " + str(months[pl_change.index(min(pl_change))+1]) + " (" + "$" + str(greatest_decrease) + ")")
 
 
-#file = open(text_path, 'w')
-#    file.write("Financial Analysis\n")
-#    file.write("---------------------\n")
- #   file.write("total months: " + str(total_months) + "\n")
-    #file.write("Total: " + "$" + str(sum(PL))+ "\n")
-    #file.write("Average Change: " + "%" + str(averageChange) + "\n")
-    #file.write("
-    #file.write("
-
+file = open(text_path, 'w')
+file.write("Financial Analysis + \n")
+file.write("--------------------- + \n")
+file.write("total months: " + str(total_months) + "\n")
+file.write("Total: " + "$" + str(sum(PL)) + "\n")
+file.write("Average Change: " + "%" + str(total_average) + "\n")
+file.write("Greatest Increase in Profits: " + str(months[pl_change.index(max(pl_change))+1]) + " (" + "$" + str(greatest_increase) + ")" + "\n")
+file.write("Greatest Decrease in Profits: " + str(months[pl_change.index(min(pl_change))+1]) + " (" + "$" + str(greatest_decrease) + ")" + "\n")
+file.close()
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,20 +3,33 @@ import csv
 
 
 csvpath = os.path.join('Resources','election_data.csv')
-
-count = []
-
 text_path = "output.txt"
 
+
+total_votes = 0
+candidates = []
+candidate_votes = []
+
+
 with open(csvpath) as csvfile:
+    csvreader = csv.DictReader(csvfile)
+ 
+    #loop through to find the total votes
+    for row in csvreader:
 
-    # CSV reader specifies delimiter and variable that holds contents
-    csvreader = csv.reader(csvfile, delimiter=',')
-    # Read the header row first
-    csv_header = next(csvreader)
+        # Find the total vote count
+        total_votes += 1
 
-#The total number of votes cast
-    total_votes = len(count)
+        candidate = row["Candidate"]
+        # if statement to run on first occurence of candidate name
+        if candidate not in candidates:
+            candidates.append(candidate)
+            candidate_votes[candidate] = 1
+        
+        candidate_votes[candidate] = candidate_votes[candidate] + 1
+
+    
+
 #A complete list of candidates who received votes
 
 #The percentage of votes each candidate won
@@ -26,6 +39,16 @@ with open(csvpath) as csvfile:
 #The winner of the election based on popular vote
 
 
-    print("Election Results")
-    print("----------------------------")
-    print("Total Votes: " + str(total_votes))
+    print("Election results")
+
+    print("--------------------------------------------------------------------------")
+
+    print("Total votes: " + str(total_votes))
+
+    print("----------------------------------------------------------------------------")
+
+ 
+    print("----------------------------------------------------------------------------------------")
+      
+    print("Winner: " + winner)
+    
