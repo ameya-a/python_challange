d297a11d352899ba48c67b87e48d5822829dc3a8
ameya-a <152837210+ameya-a@users.noreply.github.com>
2024-01-09 22:55:46 -0500

    PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,16 +1,21 @@
+# 1. Importing
+
 import os
 import csv
 
-
+# 2. setting the path
 csvpath = os.path.join('Resources','election_data.csv')
 text_path = "output.txt"
 
-
-total_votes = 0
+#3. Setting the variables
+count = 0
 candidates = []
-candidate_votes = []
+candidate_votes = {}
+winner_count = 0
+W = ""
 
 
+#4. Open the File
 with open(csvpath) as csvfile:
     csvreader = csv.DictReader(csvfile)
  
@@ -18,7 +23,7 @@ with open(csvpath) as csvfile:
     for row in csvreader:
 
         # Find the total vote count
-        total_votes += 1
+        count = count + 1
 
         candidate = row["Candidate"]
         # if statement to run on first occurence of candidate name
@@ -28,27 +33,34 @@ with open(csvpath) as csvfile:
         
         candidate_votes[candidate] = candidate_votes[candidate] + 1
 
-    
 
-#A complete list of candidates who received votes
-
-#The percentage of votes each candidate won
-
-#The total number of votes each candidate won
-
-#The winner of the election based on popular vote
-
+#5. Print the results to a text and terminal
 
     print("Election results")
+    print("-------------------------")
+    print("Total votes: " + str(count))
+    print("-------------------------")
 
-    print("--------------------------------------------------------------------------")
-
-    print("Total votes: " + str(total_votes))
-
-    print("----------------------------------------------------------------------------")
-
- 
-    print("----------------------------------------------------------------------------------------")
-      
-    print("Winner: " + winner)
+with open(text_path, 'w') as txt_file:
+    election_header = (f"Election Results\n"
+    f"---------------\n")
+   
+    txt_file.write(election_header)
     
+    for candidate in candidate_votes:
+        votes = candidate_votes[candidate]
+        vote_percentage = float(votes)/float(count)*100
+        if (votes > winner_count):
+            winner_count = votes
+            W = candidate
+        print(f"{candidate}: {vote_percentage:.3f}% ({votes})")
+        txt_file.write(f"{candidate}: {vote_percentage:.3f}% ({votes})\n")
+ 
+    print("-------------------------")
+    txt_file.write("---------------------\n")
+    print(f"Winner: {W}")
+    txt_file.write(f"Winner: {W}\n")
+    txt_file.write("---------------------\n")
+    print("-------------------------")
+    
+    txt_file.close
